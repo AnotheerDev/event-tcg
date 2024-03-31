673cee74c78c09c7d9fcb393f859bc9975a41b8d
AnotheerDev <leger.julien94@gmail.com>
2024-04-01 00:01:34 +0200

    Update authMiddleware configuration in middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,19 @@
 import { authMiddleware } from "@clerk/nextjs";
 
-export default authMiddleware({});
+export default authMiddleware({
+  publicRoutes: [
+    "/",
+    "/events/:id",
+    "/api/webhooks/clerk",
+    "/api/webhooks/stripe",
+    "/api/uploadthing",
+  ],
+  ignoredRoutes: [
+    "/api/webhooks/clerk",
+    "/api/webhooks/stripe",
+    "/api/uploadthing",
+  ],
+});
 
 export const config = {
   matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
